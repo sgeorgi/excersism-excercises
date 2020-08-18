df95f36aa822cbaff3182d663f24702b8fd99562
Sebastian Georgi <sebastian@georgi.io>
2020-08-18 10:00:41 +0200

    Worked on queen-attach a bit more

--- typescript/queen-attack/queen-attack.ts
+++ typescript/queen-attack/queen-attack.ts
@@ -1,28 +1,40 @@
-type Positions = { white: [number, number] , black: [number, number]}
+type InitialPositions = { white: Square, black: Square }
+type Square = [number, number]
+type Grid = Square[]
 
 export class QueenAttack {
-  private positions: Positions
+  private positions: InitialPositions;
+  private grid: Grid;
 
-  get white() { return this.positions.white}
-  get black() { return this.positions.black}
+  get white() {
+    return this.positions.white
+  }
 
-  constructor(positions: Positions) {
+  get black() {
+    return this.positions.black
+  }
+
+  constructor(positions: InitialPositions) {
     this.positions = positions
-    if (this.white[0] === this.black[0] && this.white[1] === this.black[1]) throw  "Queens cannot share the same space"
+    if (this.white[0] === this.black[0] && this.white[1] === this.black[1]) throw  'Queens cannot share the same space'
+
+    // initialize Grid
   }
 
   toString(): string {
     return [
-      "_ _ _ _ _ _ _ _",
-      "_ _ _ _ _ _ _ _",
-      "_ _ _ _ W _ _ _",
-      "_ _ _ _ _ _ _ _",
-      "_ _ _ _ _ _ _ _",
-      "_ _ _ _ _ _ _ _",
-      "_ _ _ _ _ _ B _",
-      "_ _ _ _ _ _ _ _\n"
-    ].join("\n")
+      '_ _ _ _ _ _ _ _',
+      '_ _ _ _ _ _ _ _',
+      '_ _ _ _ W _ _ _',
+      '_ _ _ _ _ _ _ _',
+      '_ _ _ _ _ _ _ _',
+      '_ _ _ _ _ _ _ _',
+      '_ _ _ _ _ _ B _',
+      '_ _ _ _ _ _ _ _\n'
+    ].join('\n')
   }
 
-  canAttack() { return false }
+  canAttack() {
+    return false
+  }
 }
