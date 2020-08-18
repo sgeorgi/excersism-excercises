de0fbba58b2d67799319e1c1296aa9cee3497d8d
Sebastian  Georgi <sebastian@georgi.io>
2020-08-18 08:47:28 +0200

    Worked on queen-attack

--- typescript/queen-attack/queen-attack.ts
+++ typescript/queen-attack/queen-attack.ts
@@ -11,5 +11,18 @@ export class QueenAttack {
     if (this.white[0] === this.black[0] && this.white[1] === this.black[1]) throw  "Queens cannot share the same space"
   }
 
+  toString(): string {
+    return [
+      "_ _ _ _ _ _ _ _",
+      "_ _ _ _ _ _ _ _",
+      "_ _ _ _ W _ _ _",
+      "_ _ _ _ _ _ _ _",
+      "_ _ _ _ _ _ _ _",
+      "_ _ _ _ _ _ _ _",
+      "_ _ _ _ _ _ B _",
+      "_ _ _ _ _ _ _ _\n"
+    ].join("\n")
+  }
+
   canAttack() { return false }
 }
