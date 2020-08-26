1b2e8cc97034501287142bb2f72fc112c4e84c6e
Sebastian  Georgi <sebastian@georgi.io>
2020-08-26 18:00:55 +0200

    Worked on rna transcription

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,7 +1,22 @@
-class Transcriptor {
-    toRna( /* Parameters go here */ ) {
-        // Your code here
+export class Transcriptor {
+  toRna(bases: string): string {
+    const translations = [];
+    for (let _i = 0; _i < bases.length; _i++) {
+      const char = bases[_i];
+      if (char === 'C') {
+        translations.push('G');
+      } else if (char === 'G') {
+        translations.push('C');
+      } else if (char === 'A') {
+        translations.push('U');
+      } else if (char === 'T') {
+        translations.push('A');
+      } else {
+        return '';
+      }
     }
+    return translations.join('')
+
+  }
 }
 
-export default Transcriptor
