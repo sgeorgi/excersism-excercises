c26ab20d5017471b22e4343ebfecbb31c94ce166
Sebastian  Georgi <sebastian@georgi.io>
2020-08-27 16:51:50 +0200

    Switched to switch statement and funtional mapping

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,34 +1,16 @@
 export class Transcriptor {
-  private validInput: string[] = ['C', 'G', 'A', 'T'];
-
   toRna(bases: string): string {
-    this.validate(bases);
-    return Transcriptor.translate(bases).join('');
-  }
-
-  private static translate(bases: string): string[] {
-    const translations = [];
-    for (let i = 0; i < bases.length; i++) {
-      const char = bases[i];
-      if (char === 'C') {
-        translations.push('G');
-      } else if (char === 'G') {
-        translations.push('C');
-      } else if (char === 'A') {
-        translations.push('U');
-      } else if (char === 'T') {
-        translations.push('A');
-      }
-    }
-    return translations
-  }
-
-  private validate(bases: string): void {
-    for (let i = 0; i < bases.length; i++) {
-      if (!this.validInput.includes(bases[i])) {
-        throw 'Invalid input DNA.';
-      }
-    }
+   return bases.split('')
+      .map(nucleotide => {
+        switch (nucleotide) {
+          case 'G': return 'C';
+          case 'C': return 'G';
+          case 'T': return 'A';
+          case 'A': return 'U';
+          default: throw new Error('Invalid input DNA.');
+        }
+      })
+      .join('');
   }
 }
 
